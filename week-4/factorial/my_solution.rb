81cfb3a1797fdfc1aead85ffd50940f677da53a8
[Bill Deng] <[billyd2013@gmail.com]>
2016-03-05 21:41:29 +0000

    complete 4.7 More Methods

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,27 @@
+# Factorial
+
+# I worked on this challenge [with: Evan Druce].
+
+# Your Solution Below
+
+=begin
+
+input: integer
+output: integer (total)
+
+create a variable to store the multiplications of factorial
+  use variable to multiply the given integer and store the total back to variable
+  subtract 1 with given integer
+    => repeat above 2 steps until integer becomes 1
+output the variable that we created
+
+=end
+
+def factorial(number)
+    total = 1
+    while number > 0
+      total = total * number
+      number = number - 1
+    end
+    total
+end
