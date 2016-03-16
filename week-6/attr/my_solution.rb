10f028e24c6cbb15e8718050d00b808a18d75faa
[Bill Deng] <[billyd2013@gmail.com]>
2016-03-16 05:30:04 +0000

    complete 6.4 Accessing Attributes

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -1,14 +1,35 @@
 #Attr Methods
 
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge [by myself]
 
-# I spent [#] hours on this challenge.
+# I spent [2] hours on this challenge.
+
+=begin
 
 # Pseudocode
 
-# Input:
-# Output:
+# Input: no input
+# Output: string
 # Steps:
+
+START create class 
+  create a name method that can output the name later
+  START initialize
+    create a new object to store my name
+  END initialize
+END class
+
+START create class
+  START initialize
+    create new object that will create a new object of the above class
+  END initialize
+
+  START hello method 
+    output a sentence using the above class's name method
+  END hello
+END class
+
+=end
 
 class NameData
   attr_reader :name
@@ -16,7 +37,6 @@ class NameData
     @name = "Bill"
   end
 end
-
 
 class Greetings
   def initialize
@@ -28,7 +48,8 @@ class Greetings
   end
 end
 
-
+greet = Greetings.new
+greet.hello # puts "Hello Student! How wonderful to see you today."
 
 # Reflection
 
@@ -54,28 +75,56 @@ They are using methods as setters and getters to save and retrieve profile infor
 Release 2:
 1. What changed between the last release and this release?
 
-The actual codes are shorter and from a output point of view, nothing has changed. 
+  The actual codes are shorter and from a output point of view, nothing has changed. 
 
 2. What was replaced?
 
-what_is_age method has been commented out and replaced by attr_reader :age
+  what_is_age method has been commented out and replaced by attr_reader :age
 
 3. Is this code simpler than the last?
 
-Yes, it is simpler than the last while maintaining the same output. 
+  Yes, it is simpler than the last while maintaining the same output. 
 
 Release 3:
 
 1. What changed between the last release and this release?
 
-The actual codes are even shorter and from a output point of view, nothing has changed. 
+  The actual codes are even shorter and from a output point of view, nothing has changed. 
 
 2. What was replaced?
 
-change_my_age= method has been commented out and replaced by attr_writer :age
+  change_my_age= method has been commented out and replaced by attr_writer :age
 
 3. Is this code simpler than the last?
 
-Yes, it is simpler than the last while maintaining the same output. 
+  Yes, it is simpler than the last while maintaining the same output. 
+
+Release 6:
+
+1. What is a reader method?
+
+  A reader method gets an object and outputs it.
+
+2. What is a writer method?
+
+  A writer method accepts an argument and saves the argument to an object. 
+
+3. What do the attr methods do for you?
+
+  Attr methods allow us to organize and shorten our codes. Since attr methods 
+  are usually on the top part of a method, we can see right away what setter and 
+  getter methods are available for use. 
+
+4. Should you always use an accessor to cover your bases? Why or why not?
+
+  An accessor method allows reading and writing but we should only use it if we need 
+  a method to read and write because if we don't need a method to write then it shouldn't.
+  It could lead to difficult debugging problems and it could compromise the 
+  security of our data. 
+
+5. What is confusing to you about these methods?
+
+  There is nothing confusing about these methods. I think these methods are very 
+  useful and we will be using it often in the future. 
 
 =end
